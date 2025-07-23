a0f4349b1e48b8b9ffa636daf52000f8023d6e28
vadim <ivadimn@mail.ru>
2025-07-23 20:58:57 +0300

    update db test array

--- db/main.c
+++ db/main.c
@@ -3,10 +3,13 @@
 #include "pd.h"
 #include "entity.h"
 #include "vstrex.h"
+#include "array.h"
 
 
 char *err_msg = NULL;
 
+size_t ids[] = {1, 2, 3, 4, 5, 6};
+char* names[] = {"111", "222", "333", "444", "555", "666"};
 
 
 int main(int argc, char** argv)
@@ -19,15 +22,26 @@ int main(int argc, char** argv)
     close_db(conn);
     free_conn(conn);
 
-    pd_t* pd = pd_new();
 
-    pd_ctor(pd, 100,  "Name 11");
-    printf("id = %ld\n", entity_get_id((entity_t*)pd));
-    vstr_t*  name = entity_get_name((entity_t*)pd);
-    vstr_print(name, stdout);
+    array_t* arr = new_array(A_TYPE_POINTER, 6, sizeof(pd_t*));
+    pd_t* pd = NULL;
+    for (size_t i = 0; i < 6; i++)
+    {
+        pd = pd_new();
+        pd_ctor(pd, ids[i],  names[i]);
+        put(arr, i, &pd);
+    }
     
-    pd_dtor(pd);
-    pd_free(pd);
+    for (size_t i = 0; i < 6; i++)
+    {
+        get(arr, i, &pd);
+        printf("id = %ld\n", entity_get_id((entity_t*)pd));
+        vstr_t*  name = entity_get_name((entity_t*)pd);
+        vstr_print(name, stdout);
+        pd_dtor(pd);
+        pd_free(pd);
+    }
+    del_array(arr);
     
     return 0;
 }
