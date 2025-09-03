cec37c4ba0b808f5d7b0652efff3a306a284b3c2
vadim <ivadimn@mail.ru>
2025-09-03 17:43:16 +0300

    update func

--- func/stbd.sql
+++ func/stbd.sql
@@ -8,3 +8,19 @@ CREATE TABLE users(pid SERIAL PRIMARY KEY, name TEXT NOT NULL, user_id UUID NOT 
 email TEXT NOT NULL, balance INTEGER NOT NULL, registration TIMESTAMP NOT NULL DEFAULT now());
 
 SELECT uuid_generate_v4(); 
+
+INSERT INTO users(name, email, balance) VALUES('Jerry Kinn', 'jkinn@gmail.com', 1000);
+INSERT INTO users(name, email, balance) VALUES('Adam Smith', 'asmith@gmail.com', 1000);
+INSERT INTO users(name, email, balance) VALUES('Billy Jonnes', 'bjonnes@gmail.com', 1000);
+
+ALTER TABLE users ALTER COLUMN balance SET DEFAULT 0;
+
+INSERT INTO users(name, email) VALUES('Marina Longdale', 'mlongdale@gmail.com');
+
+ALTER TABLE users ADD CHECK(balance >= 0);
+ALTER TABLE users ADD CHECK(length(name) <=100);
+ALTER TABLE users ADD CHECK(length(email) <=100);
+
+ALTER TABLE users ADD COLUMN enabled BOOLEAN NOT NULL DEFAULT true;
+
+ALTER TABLE users DROP COLUMN enabled;
